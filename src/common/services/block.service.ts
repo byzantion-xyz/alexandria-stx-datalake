a0878b2376cb6f6940e1a39562887d36619dcc94
koninbtc <konin@byzantion.xyz>
2022-07-08 13:56:23 +0200

    Increased axios timeout to 30 seconds

--- src/common/services/block.service.ts
+++ src/common/services/block.service.ts
@@ -20,7 +20,7 @@ type GetTransactionsResponse = {
   data: TransactionList;
 };
 
-const axiosOptions: AxiosRequestConfig = { timeout: 10000 };
+const axiosOptions: AxiosRequestConfig = { timeout: 30000 };
 
 export default class BlockService {
   public processBlock = async (block: StacksBlock): Promise<void> => {
