746db36f2cb8e88e55cf712b753e102c8f6cb230
Ozzy Knox <starlord@byzantion.xyz>
2022-11-02 09:54:52 +0100

    Add try/catch in main function

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,29 @@ import Application from './application';
 import { appConfig } from './common/config/app.config';
 
 (async () => {
-  const application = new Application();
+  try {
+    const application = new Application();
 
-  await application.connectDB();
+    await application.connectDB();
 
-  await application.socketSubscription();
+    await application.socketSubscription();
 
-  if (appConfig.streamHistoricalData) {
-    await application.fetchHistoricalBlocks();
-  } else {
-    console.log(
-      'Historical data stream disabled. Set ENV variable STREAM_HISTORICAL_DATA=true to enable.'
-    );
+    if (appConfig.streamHistoricalData) {
+      await application.fetchHistoricalBlocks();
+    } else {
+      console.log(
+        'Historical data stream disabled. Set ENV variable STREAM_HISTORICAL_DATA=true to enable.'
+      );
+    }
+
+    // if (appConfig.fetchHistoricalSmartContracts) {
+    //   await application.fetchHistoricalSmartContracts();
+    // }
+
+    // if (appConfig.pastBlocksToProcess) {
+    //   await application.reprocessPastBlocks();
+    // }
+  } catch (err) {
+    console.error(err);
   }
-
-  // if (appConfig.fetchHistoricalSmartContracts) {
-  //   await application.fetchHistoricalSmartContracts();
-  // }
-
-  // if (appConfig.pastBlocksToProcess) {
-  //   await application.reprocessPastBlocks();
-  // }
 })();
